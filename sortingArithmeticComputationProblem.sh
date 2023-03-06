857ceac9a7ed252898455a9ed9141baf0957d175
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-03-06 16:05:18 +0530

    Add: program inputs

--- sortingArithmeticComputationProblem.sh
+++ sortingArithmeticComputationProblem.sh
@@ -1,1 +1,14 @@
 echo "Welcome to Sorting Arithmetic Computation Problem"
+
+#!/bin/bash
+
+echo "Enter value for a:"
+read a
+
+echo "Enter value for b:"
+read b
+
+echo "Enter value for c:"
+read c
+
+echo "a = $a, b = $b, c = $c"
